5f57c74af4da45bb816d601409ce5a90a8300a62
Lanut1 <anna.tabakova27@gmail.com>
2024-06-11 16:23:41 +0200

    Refactor the code and fix handleDecimal and handleSignChange functions

--- script.js
+++ script.js
@@ -4,51 +4,143 @@ let secondNumber = '';
 let operator = '';
 let enteringNumber = false;
 
+
 input.addEventListener('click', function(event) {
     let screen = document.querySelector(".numberDisplay");
     let fullOperationScreen = document.querySelector(".operationDisplay");
-    let clearButton = document.querySelector(".clear");
+    const target = event.target;
+    const classList = target.classList;
 
-    if (event.target.classList.contains("digit")) {
-        if (firstNumber && operator && !enteringNumber) {
-            screen.textContent = '';
-            secondNumber = '';
-            enteringNumber = true;
-        } else if (firstNumber && !operator) {
-            screen.textContent = '';
-            fullOperationScreen.textContent = '';
-            firstNumber = '';
-        }
-        if (screen.textContent.slice(0, 1) === "0" && screen.textContent.length === 1) {
-            screen.textContent = '';
-            if (!operator) {
-                fullOperationScreen.textContent = '';
+    switch(true) {
+        case classList.contains("digit"):
+            handleDigit(target, screen, fullOperationScreen);
+            break;
+        case classList.contains("operator"):
+            handleOperator(target, screen, fullOperationScreen);
+            break;
+        case classList.contains("equal"):
+            handleEqual(screen, fullOperationScreen);
+            break;
+        case classList.contains("decimal"):
+            handleDecimal(screen, fullOperationScreen);
+            break;
+        case classList.contains("sign-change"):
+            handleSignChange(screen, fullOperationScreen);
+            break;
+        case classList.contains("clearAll"):
+            clearAll(screen, fullOperationScreen);
+            break;
+        case classList.contains("clear"):
+            clear(screen, fullOperationScreen);
+            break;
+    }
+});
+
+
+function operate(firstNumber, operator, secondNumber) {
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    let result;
+
+    switch(operator) {
+        case '+' :
+            result = firstNumber + secondNumber;
+            break;
+        case '-' :
+            result = firstNumber - secondNumber;
+            break;
+        case '*' :
+            result = firstNumber * secondNumber;
+            break;
+        case '/' :
+            if (secondNumber === 0) {
+                return "You cannot divide by zero!";
             }
 
-            if (operator) {
-                fullOperationScreen.textContent = fullOperationScreen.textContent.slice(0, -1);
-            }
-        }
-        screen.textContent += event.target.getAttribute('data-value');
-        fullOperationScreen.textContent += event.target.getAttribute('data-value');
+            result = firstNumber / secondNumber;
+            break;
+        case '**' :
+            result = Math.pow(firstNumber, secondNumber);
+            break;
     }
 
-    let content = screen.textContent;
-    if (event.target.classList.contains("operator") && content) {
-        if (firstNumber && operator) {
+    return result.toFixed(4);
+}
+
+
+function handleDigit(target, screen, fullOperationScreen) {
+    const maxDigits = 12;
+    if (screen.textContent.length >= maxDigits) return;
+
+    if (firstNumber && operator && !enteringNumber) {
+        screen.textContent = '';
+        secondNumber = '';
+        enteringNumber = true;
+    } else if (firstNumber && !operator) {
+        screen.textContent = '';
+        fullOperationScreen.textContent = '';
+        firstNumber = '';
+    }
+
+    if (screen.textContent.slice(0, 1) === "0" && screen.textContent.length === 1) {
+        screen.textContent = '';
+        fullOperationScreen.textContent = operator ? fullOperationScreen.textContent.slice(0, -1) : '';
+    }
+
+    screen.textContent += target.getAttribute('data-value');
+    fullOperationScreen.textContent += target.getAttribute('data-value');
+}
+
+
+function handleOperator(target, screen, fullOperationScreen) {
+    if (!screen.textContent) return;
+
+    if (firstNumber) {
+        if (operator) {
             secondNumber = screen.textContent;
             firstNumber = operate(firstNumber, operator, secondNumber);
-            screen.textContent = '';
             secondNumber = '';
-        } else if (firstNumber && !operator) {
-            screen.textContent = '';
-        } else {
-            firstNumber = screen.textContent;
-            if (firstNumber.slice(-1) === ".") {
-                firstNumber += "0";
-            }
-            screen.textContent = '';
         }
+    } else {
+        firstNumber = screen.textContent;
+
+        if (firstNumber.slice(-1) === ".") {
+            firstNumber += "0";
+        }
+    }
+
+    screen.textContent = '';
+
+    if (firstNumber === "You cannot divide by zero!") {
+        screen.textContent = firstNumber;
+        fullOperationScreen.textContent = '';
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        disableAllButtonsExcept(".clearAll");
+        return;
+    }
+
+    operator = target.getAttribute('data-value');
+
+    fullOperationScreen.textContent = firstNumber + operator;
+
+    enteringSecondNumber = false;
+    document.querySelector(".decimal").disabled = false;
+}
+
+
+function handleEqual(screen, fullOperationScreen) {
+    if (!screen.textContent) return;
+
+    if (firstNumber && operator) {
+        secondNumber = screen.textContent;
+
+        if (secondNumber.slice(-1) === ".") {
+            fullOperationScreen.textContent += "0";
+        }
+
+        firstNumber = operate(firstNumber, operator, secondNumber);
 
         if (firstNumber === "You cannot divide by zero!") {
             screen.textContent = firstNumber;
@@ -59,128 +151,76 @@ input.addEventListener('click', function(event) {
             disableAllButtonsExcept(".clearAll");
             return;
         }
-        operator = event.target.getAttribute('data-value');
-        fullOperationScreen.textContent = firstNumber + operator;
-        enteringSecondNumber = false;
-        document.querySelector(".decimal").disabled = false;
-    }
-    
-    if (event.target.classList.contains("equal") && content) {
-        if (firstNumber && operator) {
-            secondNumber = screen.textContent;
-            if (secondNumber.slice(-1) === ".") {
-                fullOperationScreen.textContent += "0";
-            }
-            firstNumber = operate(firstNumber, operator, secondNumber);
-            if (firstNumber === "You cannot divide by zero!") {
-                screen.textContent = firstNumber;
-                fullOperationScreen.textContent = '';
-                firstNumber = '';
-                secondNumber = '';
-                operator = '';
-                disableAllButtonsExcept(".clearAll");
-                return;
-            }
-            screen.textContent = firstNumber;
-            fullOperationScreen.textContent += '=' + firstNumber;
-            secondNumber = '';
-            operator = '';
-            document.querySelector(".decimal").disabled = false;
-        }
-    }
 
-    if (event.target.classList.contains("decimal")) {
-        if (!content) {
-            screen.textContent += "0.";
-            fullOperationScreen.textContent += "0.";
-            document.querySelector(".decimal").disabled = true;
-        } else {
-            screen.textContent += ".";
-            fullOperationScreen.textContent += ".";
-            document.querySelector(".decimal").disabled = true;
-        }
+        screen.textContent = firstNumber;
+        fullOperationScreen.textContent += '=' + firstNumber;
 
-        if (firstNumber && !operator) {
-            screen.textContent = '';
-            fullOperationScreen.textContent = '';
-            firstNumber = '';
-            screen.textContent += "0.";
-            fullOperationScreen.textContent += "0.";
-            document.querySelector(".decimal").disabled = true;
-        }
-    }
-
-    if (event.target.classList.contains("sign-change") && content) {
-        if (operator === "+") {
-            operator = "-";
-            fullOperationScreen.textContent = firstNumber + operator + content;
-        } else if (operator === "-") {
-            operator = "+";
-            fullOperationScreen.textContent = firstNumber + operator + content;
-        } else if (operator) {
-            if (screen.textContent.startsWith("-")) {
-                screen.textContent = screen.textContent.slice(1);
-                fullOperationScreen.textContent = firstNumber + operator + screen.textContent;
-            } else {
-                screen.textContent = "-" + screen.textContent;
-                fullOperationScreen.textContent = firstNumber + operator + screen.textContent;
-            }
-        } else {
-            if (screen.textContent.startsWith("-")) {
-                screen.textContent = screen.textContent.slice(1);
-                firstNumber = screen.textContent;
-                fullOperationScreen.textContent = screen.textContent;
-            } else {
-                screen.textContent = "-" + screen.textContent;
-                firstNumber = screen.textContent;
-                fullOperationScreen.textContent = screen.textContent;
-            }
-        }
-    }
-
-    if (event.target.classList.contains("clearAll")) {
-        screen.textContent = '';
-        fullOperationScreen.textContent = '';
-        firstNumber = '';
         secondNumber = '';
         operator = '';
-        enableAllButtons();
+        document.querySelector(".decimal").disabled = false;
+    }
+}
+
+
+function handleDecimal(screen, fullOperationScreen) {
+    fullOperationScreen.textContent += screen.textContent ? "." : "0.";
+    screen.textContent += screen.textContent ? "." : "0.";
+
+    if (firstNumber && operator) {
+        enteringNumber = true;
+    }
+
+    if (firstNumber && !operator) {
+        firstNumber = '';
+        screen.textContent = "0.";
+        fullOperationScreen.textContent = "0.";
+    }
+
+    document.querySelector(".decimal").disabled = true;
+}
+
+
+function handleSignChange(screen, fullOperationScreen) {
+    if (!screen.textContent) return;
+
+    if (operator === "+" || operator === "-") {
+        operator = operator === "+" ? "-" : "+";
+    } else {
+        screen.textContent = screen.textContent.startsWith("-") ? screen.textContent.slice(1) : "-" + screen.textContent;
+    }
+
+    if (operator) {
+        fullOperationScreen.textContent = firstNumber + operator + screen.textContent;
+    } else {
+        fullOperationScreen.textContent = screen.textContent;
+    }
+}
+
+
+function clearAll(screen, fullOperationScreen) {
+    screen.textContent = '';
+    fullOperationScreen.textContent = '';
+
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+
+    enableAllButtons();
+    document.querySelector(".decimal").disabled = false;
+}
+
+
+function clear(screen, fullOperationScreen) {
+    if (!screen.textContent) return;
+
+    if (firstNumber && !operator) return;
+
+    if (screen.textContent.slice(-1) === ".") {
         document.querySelector(".decimal").disabled = false;
     }
 
-    if (event.target.classList.contains("clear") && content) {
-        if (firstNumber && !operator) {
-            return;
-        }
-
-        if (content.slice(-1) === ".") {
-            document.querySelector(".decimal").disabled = false;
-        }
-        screen.textContent = screen.textContent.slice(0, -1);
-        fullOperationScreen.textContent = fullOperationScreen.textContent.slice(0, -1);
-    }
-
-});
-
-function operate(firstNumber, operator, secondNumber) {
-    firstNumber = parseFloat(firstNumber);
-    secondNumber = parseFloat(secondNumber);
-
-    switch(operator) {
-        case '+' :
-            return firstNumber + secondNumber;
-        case '-' :
-            return firstNumber - secondNumber;
-        case '*' :
-            return firstNumber * secondNumber;
-        case '/' :
-            if (secondNumber === 0) {
-                return "You cannot divide by zero!";
-            }
-            return firstNumber / secondNumber;
-        case '**' :
-            return Math.pow(firstNumber, secondNumber);
-    }
+    screen.textContent = screen.textContent.slice(0, -1);
+    fullOperationScreen.textContent = fullOperationScreen.textContent.slice(0, -1);
 }
 
 
